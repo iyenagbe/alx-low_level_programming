449cbaf1fce2245820d9d852711c16639e430d10
iyenagbe <iyenagberaymond@gmail.com>
2023-09-12 15:12:16 -0700

    /**

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,32 +1,40 @@
 #include <stdio.h>
 
 /**
- * main - main block
+ * main - prints the first 98 fibonaci sequences
  *
- * Description: compute and print the sum of all the multiples of 3 or
- *
- * 5 below 1024 (excluded), followed by new line
- *
- * Return: 0 (ipdated)
+ * Return: 0 (updated)
  */
 
 int main(void)
 {
-	int i < 98;
-	unsigned long int q = 0, b = 1, next = 0;
+	unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000, f1o = 0, f2o = 0, tmpo = 0;
+	short int i = 1, initial0s;
 
-	while (i < 98)
+	while (i <= 98)
 	{
-	next = a + b;
-	b = next;
-	printf("%lu", next);
+		if (f1o > 0)
+			printf("%lu", f1o);
+		initial0s = numLength(mx) - 1 - numLength(f1);
+		while (f1o > 0 && initial0s > 0)
+		{
+			printf("%i", 0);
+			initial0s--;
+		}
+		printf("%lu", f1);
 
-	if (i < 97)
-		printf(",");
-	i++;
+		tmp = (f1 + f2) % mx;
+		tmpo = f1o + f2o + (f1 + f2) / mx;
+		f1 = f2;
+		f1o = f2o;
+		f2 = tmp;
+		f2o = tmpo;
+
+		if (i != 98)
+			printf(", ");
+		else
+			printf("\n");
+		i++;
 	}
-
-	putchar('\n');
-
-	return(0);
+	return (0);
 }
