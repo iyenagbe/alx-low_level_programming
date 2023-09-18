cd93014e6a6a0b346a18278e242a91b5e1254da0
iyenagbe <iyenagberaymond@gmail.com>
2023-09-18 09:01:30 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,5 +8,18 @@
 
 void rev_string(char *s)
 {
+	int i;
+	int count = 0;
 
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+
+	for (i = 0; i  < count / 2; i++)
+	{
+		char c;
+
+		c = s[i];
+	s[i] = s[count - 1 - i];
+	s[count - 1 - i] = c;
+	}
 }
