fd67ba0933dc254d8eb6236c493f34b60c58d7df
iyenagbe <iyenagberaymond@gmail.com>
2023-09-18 08:12:04 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - print reverce string
+ * @s: printed string
+ * Return: void
+ */
+
+	void print_rev(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\n'; i++)
+		count++;
+	for (i = count = 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
