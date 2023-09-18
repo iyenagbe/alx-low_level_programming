ad7ccff3d22823c898dd1b432dc8c5ff40a336a3
iyenagbe <iyenagberaymond@gmail.com>
2023-09-18 10:05:15 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,13 +9,15 @@
 	void puts_half(char *str)
 
 {
-	int c, n;
+	int c;
+	int n;
 	int count = 0;
 
 	for (c = 0; str[c] != '\0'; c++)
 		count++;
 
-	n = count - 1 / 2;
+		n = (count - 1) / 2;
+
 	for (c = n + 1; str[c] != '\0'; c++)
 		_putchar(str[c]);
 
